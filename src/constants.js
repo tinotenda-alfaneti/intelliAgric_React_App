889c3a1c4f18592a2288ac55b655ab1c5bc95279
bethelchoto <chotobethel68@gmail.com>
2024-07-02 10:32:12 +0000

    link update

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-// export const API_BASE_URL = "https://intelliagric-capstone.onrender.com/";
-export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL = "https://intelliagric-capstone.onrender.com/";
+// export const API_BASE_URL = "http://127.0.0.1:5000";
 
 
 export const ENDPOINTS = {
