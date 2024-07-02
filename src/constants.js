690bdb9df44900c4944407c06a2fc787a65815f7
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2024-07-02 14:09:35 +0000

    Edit base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-export const API_BASE_URL = "https://intelliagric-capstone.onrender.com/";
+export const API_BASE_URL = "https://intelliagric-capstone.onrender.com";
 // export const API_BASE_URL = "http://127.0.0.1:5000";
 
 
